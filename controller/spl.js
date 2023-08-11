cd924bd95aeb51c1300a068a10be4ac18fa66c60
amer200 <amermostaafa@gmail.com>
2023-08-11 18:39:05 +0300

    commit

--- controller/spl.js
+++ controller/spl.js
@@ -132,7 +132,7 @@ exports.creteNewOrder = async (req, res) => {
             })
         })
         .catch(err => {
-            console.log(err.statusText)
+            console.log(err.response)
         })
 
 }
