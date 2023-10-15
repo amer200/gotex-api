be8c38faf2a94ce19649f0004b94f5d4ae5c814c
amer200 <amermostaafa@gmail.com>
2023-10-15 13:40:11 +0300

    make get-all-client allowed for all

--- routes/client.js
+++ routes/client.js
@@ -4,7 +4,7 @@ const userMiddlewares = require('../middleware/user');
 const clientController = require("../controller/clients/clients");
 
 routes.post("/add-new-client", userMiddlewares.isAuth, userMiddlewares.isMarkter, clientController.addClient);
-routes.get("/get-all-clients", userMiddlewares.isAuth, userMiddlewares.isMarkter, clientController.getAllClients);
+routes.get("/get-all-clients", userMiddlewares.isAuth, clientController.getAllClients);
 routes.post("/edit-client/:id", userMiddlewares.isAuth, userMiddlewares.isMarkter, clientController.editClient);
 
 module.exports = routes;
