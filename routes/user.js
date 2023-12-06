f8cbe4c5acf39f3fd1b46b2a0283a31833454007
Soha <sohaali710@gmail.com>
2023-12-06 22:38:50 +0200

    Update

--- routes/user.js
+++ routes/user.js
@@ -3,23 +3,25 @@ const routes = express.Router();
 const userControllers = require("../controller/user");
 const clintControllers = require("../controller/clints");
 const userMiddlewares = require('../middleware/user');
-const chek = require("../middleware/new-client");
+const check = require("../middleware/new-client");
+
 routes.post('/signup', userMiddlewares.isValide, userControllers.signUp);
-routes.get("/activate-user/:code/:id", userControllers.activateUser);
 routes.post('/marketer-signup', userMiddlewares.isValide, userControllers.MarkterSignUp);
+routes.get("/activate-user/:code/:id", userControllers.activateUser);
 routes.post('/login', userControllers.logIn);
-routes.get('/get-user-balance', userMiddlewares.isAuth, userControllers.getUserBalance);
 routes.get("/resend-activate-code", userMiddlewares.isAuth, userControllers.reSendActivateCode);
 routes.post("/send-email-update-password", userControllers.createNewPassword);
 routes.post("/update-password", userControllers.updatePassword);
-routes.post("/add-new-clint", userMiddlewares.isAuth, userMiddlewares.isMarkter, clintControllers.addNewClint);
-routes.post("/add-clint-deposit", userMiddlewares.isAuth, userMiddlewares.isMarkter, clintControllers.adddeposit);
-routes.get("/all-markter-clint", userMiddlewares.isAuth, userMiddlewares.isMarkter, clintControllers.getAllMarkterClint);
 
 routes.post("/add-user-balance", userMiddlewares.isAuth, userControllers.addBalance);
+routes.get('/get-user-balance', userMiddlewares.isAuth, userControllers.getUserBalance);
 routes.get("/checkpayment/:status/:uId/:code", userControllers.checkPaymentOrder);
 routes.get("/get-all-payment-orders", userMiddlewares.isAuth, userControllers.getAllPaymentOrders);
 
+/** By Marketer */
+routes.post("/add-new-clint", userMiddlewares.isAuth, userMiddlewares.isMarkter, clintControllers.addNewClint);
+routes.post("/add-clint-deposit", userMiddlewares.isAuth, userMiddlewares.isMarkter, clintControllers.adddeposit);
+routes.get("/all-markter-clint", userMiddlewares.isAuth, userMiddlewares.isMarkter, clintControllers.getAllMarkterClint);
 routes.post("/add-credit-to-client", userMiddlewares.isAuth, userMiddlewares.isMarkter, userControllers.addCreditsToClient);
 
 module.exports = routes
