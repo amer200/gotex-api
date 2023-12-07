7720e2f34ce4cd1c5042be0d7918586f30ec13f3
Soha <sohaali710@gmail.com>
2023-12-07 22:33:06 +0200

    User charge - tap payment gateway

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = express.Router();
 const userControllers = require("../controller/user");
+const paymentControllers = require("../controller/payment/main");
 const clintControllers = require("../controller/clints");
 const userMiddlewares = require('../middleware/user');
 const check = require("../middleware/new-client");
@@ -14,9 +15,14 @@ routes.post("/send-email-update-password", userControllers.createNewPassword);
 routes.post("/update-password", userControllers.updatePassword);
 
 routes.post("/add-user-balance", userMiddlewares.isAuth, userControllers.addBalance);
-routes.get('/get-user-balance', userMiddlewares.isAuth, userControllers.getUserBalance);
 routes.get("/checkpayment/:status/:uId/:code", userControllers.checkPaymentOrder);
+routes.get('/get-user-balance', userMiddlewares.isAuth, userControllers.getUserBalance);
 routes.get("/get-all-payment-orders", userMiddlewares.isAuth, userControllers.getAllPaymentOrders);
+
+/** user payment [with tap gateway] */
+routes.post("/user-charge", userMiddlewares.isAuth, paymentControllers.userCharge);
+routes.get("/check-tap-payment/:status/:userId/:code", paymentControllers.checkPaymentOrder);
+routes.get("/get-user-payment-orders", userMiddlewares.isAuth, paymentControllers.getUserPaymentOrders);
 
 /** By Marketer */
 routes.post("/add-new-clint", userMiddlewares.isAuth, userMiddlewares.isMarkter, clintControllers.addNewClint);
