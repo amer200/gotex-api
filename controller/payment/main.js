72a3ed73bce1d452ba14403e2cd912c5ab713595
amer200 <amermostaafa@gmail.com>
2023-12-10 18:27:33 +0200

    commit

--- controller/payment/main.js
+++ controller/payment/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const User = require("../../model/user");
 const PaymentOrder = require("../../model/payment/orders");
 const axios = require("axios");
@@ -175,4 +176,86 @@ exports.getUserPaymentOrders = async (req, res) => {
             error: err.message
         })
     }
+=======
+const axios = require('axios');
+const genRandomString = require("../../modules/genRandomString");
+const User = require("../../model/user");
+const paymentOrder = require("../../model/payment/orders");
+exports.addDepoist = async (req, res) => {
+    const amount = req.body.amount;
+    const firstName = req.body.first_name;
+    const middleName = req.body.middle_name;
+    const lastName = req.body.last_name;
+    const email = req.body.email;
+    const countryCode = req.body.country_code;
+    const phone = req.body.phone;
+    const authCode = genRandomString(10);
+    const userId = req.user.user.id;
+    let data = JSON.stringify({
+        "amount": amount,
+        "currency": "SAR",
+        "customer_initiated": true,
+        "threeDSecure": true,
+        "save_card": false,
+        "description": "Test Description",
+        "metadata": {
+            "udf1": "Metadata 1"
+        },
+        "reference": {
+            "transaction": "txn_01",
+            "order": "ord_01"
+        },
+        "receipt": {
+            "email": true,
+            "sms": true
+        },
+        "customer": {
+            "first_name": firstName,
+            "middle_name": middleName,
+            "last_name": lastName,
+            "email": email,
+            "phone": {
+                "country_code": countryCode,
+                "number": phone
+            }
+        },
+        "merchant": {
+            "id": "1234"
+        },
+        "source": {
+            "id": "src_all"
+        },
+        "redirect": {
+            "url": `http://dashboard.go-tex.net`
+        }
+    });
+
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://api.tap.company/v2/charges',
+        headers: {
+            'Authorization': 'Bearer sk_test_iN3MadpErZUhYeIV9WCvXOo4',
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+    try {
+        const response = await axios.request(config);
+        const order = new paymentOrder({
+            user: userId,
+            data: response.data,
+            amount: amount,
+            code: authCode,
+            status: "pinding"
+        })
+        await order.save();
+        res.status(200).json({
+            data: response.data
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+>>>>>>> f054960 (commit)
 }
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 const userControllers = require("../controller/user");
 const paymentControllers = require("../controller/payment/main");
 const clintControllers = require("../controller/clints");
+const paymentContollers = require("../controller/payment/main")
 const userMiddlewares = require('../middleware/user');
 const check = require("../middleware/new-client");
 
@@ -15,8 +16,17 @@ routes.post("/send-email-update-password", userControllers.createNewPassword);
 routes.post("/update-password", userControllers.updatePassword);
 
 routes.post("/add-user-balance", userMiddlewares.isAuth, userControllers.addBalance);
+<<<<<<< HEAD
 routes.get("/checkpayment/:status/:uId/:code", userControllers.checkPaymentOrder);
 routes.get('/get-user-balance', userMiddlewares.isAuth, userControllers.getUserBalance);
+=======
+<<<<<<< HEAD
+routes.get('/get-user-balance', userMiddlewares.isAuth, userControllers.getUserBalance);
+routes.get("/checkpayment/:status/:uId/:code", userControllers.checkPaymentOrder);
+=======
+// routes.get("/checkpayment/:status/:uId/:code", userControllers.checkPaymentOrder);
+>>>>>>> 3165ba8 (payment tap)
+>>>>>>> f054960 (commit)
 routes.get("/get-all-payment-orders", userMiddlewares.isAuth, userControllers.getAllPaymentOrders);
 
 /** user payment [with tap gateway] */
@@ -29,5 +39,6 @@ routes.post("/add-new-clint", userMiddlewares.isAuth, userMiddlewares.isMarkter,
 routes.post("/add-clint-deposit", userMiddlewares.isAuth, userMiddlewares.isMarkter, clintControllers.adddeposit);
 routes.get("/all-markter-clint", userMiddlewares.isAuth, userMiddlewares.isMarkter, clintControllers.getAllMarkterClint);
 routes.post("/add-credit-to-client", userMiddlewares.isAuth, userMiddlewares.isMarkter, userControllers.addCreditsToClient);
-
+/******tap */
+routes.post("/request-payment", userMiddlewares.isAuth, paymentContollers.addDepoist);
 module.exports = routes
