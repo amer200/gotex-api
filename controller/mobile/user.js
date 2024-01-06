d57cb2376439cb15c5d35500a91737290c90dc02
Soha Ali <sohaali710@gmail.com>
2024-01-07 00:51:15 +0200

    Sing up for mobile app

---
+++ controller/mobile/user.js
@@ -0,0 +1,53 @@
+const User = require("../../model/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
+const salt = 10;
+const sendEmail = require("../../modules/sendEmail");
+const genRandomString = require("../../modules/genRandomString");
+const mailSubject = "Verify your gotex account"
+
+exports.signUp = (req, res) => {
+    const { name, password, email, mobile, address, location } = req.body;
+    var cr = []
+    if (req.files) {
+        req.files.forEach(f => {
+            cr.push(f.path)
+        });
+    }
+    const hash = bcrypt.hashSync(password, salt);
+    User.findOne({ email: email })
+        .then(u => {
+            if (u) {
+                res.status(200).json({
+                    msg: "error this email is already used"
+                })
+            } else {
+                const user = new User({
+                    name: name,
+                    password: hash,
+                    email: email,
+                    mobile: mobile,
+                    address: address,
+                    location: location,
+                    verified: false,
+                    emailcode: genRandomString(6),
+                    rolle: "user",
+                    cr: cr
+                })
+                user.save()
+                    .then(u => {
+                        sendEmail(u.email, u.emailcode, u._id, "/../views/emailTempForMobile.ejs", mailSubject);
+                        res.status(200).json({
+                            msg: "ok",
+                            user: u
+                        })
+                    })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                msg: err.message
+            })
+        })
+}
