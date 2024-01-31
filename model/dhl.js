ef42587d3eb1e4afcd82f6c4ad46645915f0f0ab
Soha Ali <sohaali710@gmail.com>
2024-01-31 21:18:55 +0200

    DHL routes

---
+++ model/dhl.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const dhlSchema = mongoose.Schema({
+    name: {
+        type: String,
+        default: "dhl"
+    },
+    userprice: {
+        type: Number,
+        default: 22,
+    },
+    marketerprice: {
+        type: Number,
+        default: 22,
+    },
+    kgprice: {
+        type: Number,
+        default: 22,
+    },
+    status: {
+        type: Boolean,
+        default: false,
+    },
+    codprice: {
+        type: Number,
+        default: 10
+    },
+    mincodmarkteer: {
+        type: Number,
+        default: 10
+    },
+    maxcodmarkteer: {
+        type: Number,
+        default: 10
+    },
+    daftraId: String
+})
+
+module.exports = mongoose.model("Dhl", dhlSchema);
---
+++ model/dhlorders.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const dhlOrderSchema = mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    company: String,
+    ordernumber: String,
+    data: Object,
+    paytype: String,
+    price: Number,
+    marktercode: {
+        type: String,
+        default: ''
+    },
+    createdate: Date,
+    created_at: Date,
+    inovicedaftra: Object,
+    supplier_inovicedaftra: Object,
+    status: {
+        type: String,
+        enum: ['failed', 'pending', 'accepted', 'canceled'],
+        default: 'pending'
+    },
+    cancelReason: String
+})
+
+module.exports = mongoose.model("DhlOrder", dhlOrderSchema);
