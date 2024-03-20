3496c98470307968b8caff943b774dbab714516b
Soha Ali <sohaali710@gmail.com>
2024-03-20 20:44:53 +0200

    Filter orders by date and company

--- routes/orders.js
+++ routes/orders.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = express.Router();
 const { isAuth } = require("../middleware/user");
 const { isAuth: isAdminAuth } = require("../middleware/admin");
-const { getAllOrders, getOrders, getOrderById, getCancelOrderRequests, cancelOrderRequest, cancelOrderRequestStatus } = require("../controller/orders");
+const { getAllOrders, getOrders, getOrderById, getCancelOrderRequests, cancelOrderRequest, cancelOrderRequestStatus, filterOrdersByDate } = require("../controller/orders");
 
 routes.get('/get-cancel-order-requests', isAdminAuth, getCancelOrderRequests);
 routes.post("/cancel-order-request", isAuth, cancelOrderRequest);
@@ -10,6 +10,7 @@ routes.post("/cancel-order-request-status", isAdminAuth, cancelOrderRequestStatu
 
 // routes.get('/all', isAdminAuth, getAllOrders);
 routes.get('/', isAdminAuth, getOrders);
+routes.get('/filter-by-date', isAdminAuth, filterOrdersByDate);
 routes.get('/:orderId', isAdminAuth, getOrderById);
 
 
