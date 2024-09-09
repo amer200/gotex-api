f2f92bd968f47e56abd7ffbaf13044f607b87b44
Soha Ali <sohaali710@gmail.com>
2024-09-09 04:46:33 +0300

    Handle get user orders route

--- routes/orders.js
+++ routes/orders.js
@@ -2,16 +2,29 @@ const express = require("express");
 const routes = express.Router();
 const { isAuth } = require("../middleware/user");
 const { isAuth: isAdminAuth } = require("../middleware/admin");
-const { getAllOrders, getOrders, getOrderById, getCancelOrderRequests, cancelOrderRequest, cancelOrderRequestStatus, filterOrdersByDate } = require("../controller/orders");
+const {
+  getAllOrders,
+  getOrders,
+  getOrderById,
+  getCancelOrderRequests,
+  cancelOrderRequest,
+  cancelOrderRequestStatus,
+  filterOrdersByDate,
+  getUserOrders,
+} = require("../controller/orders");
 
-routes.get('/get-cancel-order-requests', isAdminAuth, getCancelOrderRequests);
+routes.get("/get-cancel-order-requests", isAdminAuth, getCancelOrderRequests);
 routes.post("/cancel-order-request", isAuth, cancelOrderRequest);
-routes.post("/cancel-order-request-status", isAdminAuth, cancelOrderRequestStatus);
+routes.post(
+  "/cancel-order-request-status",
+  isAdminAuth,
+  cancelOrderRequestStatus
+);
 
 // routes.get('/all', isAdminAuth, getAllOrders);
-routes.get('/', isAdminAuth, getOrders);
-routes.get('/filter-by-date', isAdminAuth, filterOrdersByDate);
-routes.get('/:orderId', isAdminAuth, getOrderById);
-
+routes.get("/", isAdminAuth, getOrders);
+routes.get("/user-orders", isAuth, getUserOrders);
+routes.get("/filter-by-date", isAdminAuth, filterOrdersByDate);
+routes.get("/:orderId", isAdminAuth, getOrderById);
 
 module.exports = routes;
