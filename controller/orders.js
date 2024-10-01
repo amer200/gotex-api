df1a01a9ff43b491be6309df792bafbcdce8f286
Soha Ali <sohaali710@gmail.com>
2024-10-01 16:32:40 +0300

    Refactor pagination in get orders

--- controller/orders.js
+++ controller/orders.js
@@ -11,87 +11,16 @@ const GltOrder = require("../model/gltorders");
 const SaeeOrder = require("../model/saeeorders");
 const SmsaOrder = require("../model/smsaorders");
 const SplOrder = require("../model/splorders");
+
+const {
+  countDocsAfterFiltering,
+  createPaginationObj,
+} = require("../modules/pagination");
+
 /**
  * @Desc :  Filter with company, paytype, billCode, marktercode or keyword (user data -> name, email or mobile)
- *        + Filter by date
  *        + Pagination
  */
-// TODO: Add Glt orders
-// exports.getAllOrders = async (req, res) => {
-//     /** Pagination -> default: page=1, limit=30 (max number of items (orders) per page)*/
-//     let page = +req.query.page || 1
-//     const limit = +req.query.limit || 30
-//     const skip = (page - 1) * limit
-//     const startDate = req.query.startDate || new Date('2000-01-01')
-//     const endDate = req.query.endDate || new Date()
-//     const { company = '', paytype = '', billCode = '', marktercode = '', keyword = '' } = req.query
-
-//     const { company = '', paytype = '', billCode = '', marktercode = '', keyword = '' } = req.query
-
-//     try {
-//         console.time('blocking await')
-//         const query = {
-//             paytype: { $regex: paytype, $options: 'i' },// $options: 'i' to make it case-insensitive (accept capital or small chars)
-//             marktercode: { $regex: marktercode, $options: 'i' },
-//             company: { $regex: company, $options: 'i' },
-//             billCode: { $regex: billCode, $options: 'i' },
-//             billCode: { $regex: billCode, $options: 'i' },
-//             created_at: {
-//                 $gte: startDate,
-//                 $lte: endDate
-//             },
-//             status: { $ne: "failed" },
-//         }
-//         const populateObj = {
-//             path: 'user',
-//             /**@Desc if users.name or user.email != keyword, it returns user=null */
-//             match: {
-//                 $or: [
-//                     { name: { $regex: keyword, $options: 'i' } },
-//                     { email: { $regex: keyword, $options: 'i' } },
-//                     { mobile: { $regex: keyword, $options: 'i' } }
-//                 ]
-//             },
-//             select: "name email mobile"
-//         }
-
-//         let ordersPerPage = await Order.find(query)
-//             .limit(limit).skip(skip).sort({ created_at: -1 })
-//             .populate(populateObj)
-//             .select("-__v -data.waybills -data.data.imileAwb -data.sticker -data.Shipments.ShipmentDetails");
-//             .populate(populateObj)
-//             .select("-__v -data.waybills -data.data.imileAwb -data.sticker -data.Shipments.ShipmentDetails");
-
-//         let numberOfOrders = ''
-//         let allOrders = []
-//         if (keyword) {
-//             ordersPerPage = ordersPerPage.filter(ordersPerPage => ordersPerPage.user) // filter orders to remove user=null
-//             allOrders = await Order.find(query).populate(populateObj);
-//             numberOfOrders = allOrders.filter(allOrders => allOrders.user).length
-//         } else {
-//             numberOfOrders = await Order.find(query).countDocuments()
-//         }
-
-//         const numberOfPages = (numberOfOrders % limit == 0) ? numberOfOrders / limit : Math.floor(numberOfOrders / limit) + 1;
-
-//         console.timeEnd('blocking await')
-//         res.status(200).json({
-//             result: ordersPerPage.length,
-//             pagination: {
-//                 currentPage: page,
-//                 limit,
-//                 numberOfPages
-//             },
-//             data: ordersPerPage
-//         })
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json({
-//             error: err.message
-//         })
-//     }
-// }
-
 exports.getOrders = async (req, res) => {
   /**Pagination -> default: page=1, limit=30 (max number of items (orders) per page)*/
   let page = +req.query.page || 1;
@@ -106,34 +35,37 @@ exports.getOrders = async (req, res) => {
   } = req.query;
 
   try {
-    console.time("block");
-    let matchObj = {};
+    let matchStage = {};
     if (!company && !paytype && !billCode && !marktercode && !keyword) {
-      matchObj = {};
+      matchStage = { $match: {} };
     } else if (!keyword) {
-      matchObj = {
-        paytype: { $regex: paytype, $options: "i" }, // $options: 'i' to make it case-insensitive (accept capital or small chars)
-        marktercode: { $regex: marktercode, $options: "i" },
-        company: { $regex: company, $options: "i" },
-        billCode: { $regex: billCode, $options: "i" },
-        status: { $ne: "failed" },
+      matchStage = {
+        $match: {
+          paytype: { $regex: paytype, $options: "i" }, // $options: 'i' to make it case-insensitive (accept capital or small chars)
+          marktercode: { $regex: marktercode, $options: "i" },
+          company: { $regex: company, $options: "i" },
+          billCode: { $regex: billCode, $options: "i" },
+          status: { $ne: "failed" },
+        },
       };
     } else {
-      matchObj = {
-        paytype: { $regex: paytype, $options: "i" }, // $options: 'i' to make it case-insensitive (accept capital or small chars)
-        marktercode: { $regex: marktercode, $options: "i" },
-        company: { $regex: company, $options: "i" },
-        billCode: { $regex: billCode, $options: "i" },
-        status: { $ne: "failed" },
-        $or: [
-          { "user.name": { $regex: keyword, $options: "i" } },
-          { "user.email": { $regex: keyword, $options: "i" } },
-          { "user.mobile": { $regex: keyword, $options: "i" } },
-        ],
+      matchStage = {
+        $match: {
+          paytype: { $regex: paytype, $options: "i" }, // $options: 'i' to make it case-insensitive (accept capital or small chars)
+          marktercode: { $regex: marktercode, $options: "i" },
+          company: { $regex: company, $options: "i" },
+          billCode: { $regex: billCode, $options: "i" },
+          status: { $ne: "failed" },
+          $or: [
+            { "user.name": { $regex: keyword, $options: "i" } },
+            { "user.email": { $regex: keyword, $options: "i" } },
+            { "user.mobile": { $regex: keyword, $options: "i" } },
+          ],
+        },
       };
     }
 
-    const ordersPromise = Order.aggregate([
+    const lookupStages = [
       {
         $lookup: {
           // populate with user data
@@ -151,9 +83,11 @@ exports.getOrders = async (req, res) => {
           as: "marketer",
         },
       },
-      {
-        $match: matchObj,
-      },
+    ];
+
+    const ordersPerPage = await Order.aggregate([
+      ...lookupStages,
+      matchStage,
       { $sort: { created_at: -1 } },
       { $skip: skip },
       { $limit: limit },
@@ -190,95 +124,18 @@ exports.getOrders = async (req, res) => {
       },
     ]);
 
-    // to count number of filtered orders
-    const allOrdersPromise = Order.aggregate([
-      {
-        $lookup: {
-          from: "users",
-          localField: "user",
-          foreignField: "_id",
-          as: "user",
-        },
-      },
-      {
-        $match: matchObj,
-      },
-      { $count: "totalCount" },
-    ]);
-    const [orders, allOrders] = await Promise.all([
-      ordersPromise,
-      allOrdersPromise,
-    ]);
+    // To calculate pagination info
+    const totalCount = await countDocsAfterFiltering(
+      Order,
+      matchStage,
+      lookupStages
+    );
+    const pagination = createPaginationObj(page, limit, totalCount);
 
-    // const myOrders = await Order.find({})
-    // myOrders.forEach(async (order) => {
-    //     switch (order.company) {
-    //         case 'anwan':
-    //             order.billCode = order.data.awb_no
-    //             break;
-    //         case 'aramex':
-    //             console.log(order.data.Shipments[0])
-    //             if (order.data.Shipments[0]) {
-    //                 order.billCode = order.data.Shipments["ID"]
-    //             }
-    //             break;
-    //         case 'imile':
-    //             if (order.data.code == "200") {
-    //                 order.billCode = order.data.data.expressNo
-    //             }
-    //             break;
-    //         case 'jt':
-    //             if (order.data.msg == "success") {
-    //                 console.log(order.data.data.billCode)
-    //                 order.billCode = order.data.data.billCode
-    //             }
-    //             break;
-    //         case 'saee':
-    //             order.billCode = order.data.waybill
-    //             break;
-    //         case 'smsa':
-    //             order.billCode = order.data.sawb
-    //             break;
-    //         case 'Spl':
-    //            if(orders.data.Items){
-    //             order.billCode = order.data.Items[0]?.Barcode
-    //            }
-    //             break;
-    //         default:
-    //             break;
-    //     }
-    //     order.billCode = order.billCode || ''
-    //     await order.save()
-    // })
-
-    // const myOrders = await Order.find({ company: "saee" });
-    // const companyOrders = await SaeeOrder.find();
-    // myOrders.forEach(async (order1) => {
-    //   companyOrders.forEach(async (order2) => {
-    //     order1.marktercode = order2.marktercode;
-    //     await order1.save();
-    //   });
-    // });
-
-    let numberOfOrders,
-      numberOfPages = 0;
-    if (allOrders[0]) {
-      numberOfOrders = allOrders[0].totalCount;
-      numberOfPages =
-        numberOfOrders % limit == 0
-          ? numberOfOrders / limit
-          : Math.floor(numberOfOrders / limit) + 1;
-    }
-
-    console.timeEnd("block");
     res.status(200).json({
-      result: orders.length,
-      pagination: {
-        currentPage: page,
-        limit,
-        numberOfPages,
-      },
-      data: orders,
+      result: ordersPerPage.length,
+      pagination,
+      data: ordersPerPage,
     });
   } catch (err) {
     console.log(err);
@@ -297,9 +154,9 @@ exports.getUserOrders = async (req, res) => {
   const { company = "", billCode = "", marktercode = "" } = req.query;
 
   try {
-    let matchObj = { user: new mongoose.Types.ObjectId(userId) };
+    let matchStage = { user: new mongoose.Types.ObjectId(userId) };
     if (company || billCode || marktercode) {
-      matchObj = {
+      matchStage = {
         user: new mongoose.Types.ObjectId(userId),
         company: { $regex: company, $options: "i" },
         billCode: { $regex: billCode, $options: "i" },
@@ -309,7 +166,7 @@ exports.getUserOrders = async (req, res) => {
 
     const ordersPromise = Order.aggregate([
       {
-        $match: matchObj,
+        $match: matchStage,
       },
       { $sort: { created_at: -1 } },
       { $skip: skip },
@@ -329,7 +186,7 @@ exports.getUserOrders = async (req, res) => {
 
     const allOrdersPromise = Order.aggregate([
       {
-        $match: matchObj,
+        $match: matchStage,
       },
       { $count: "totalCount" },
     ]);
---
+++ modules/pagination.js
@@ -0,0 +1,31 @@
+/**
+ * @Desc : create object of pagination details (using aggregate)
+ */
+exports.countDocsAfterFiltering = async (
+  Model,
+  matchStage,
+  lookupStages = []
+) => {
+  const result = await Model.aggregate([
+    ...lookupStages,
+    matchStage,
+    { $count: "totalCount" },
+  ]);
+
+  return result[0]?.totalCount || 0;
+};
+
+exports.createPaginationObj = (page, limit, totalCount) => {
+  const numberOfPages =
+    totalCount % limit == 0
+      ? totalCount / limit
+      : Math.floor(totalCount / limit) + 1;
+
+  const pagination = {
+    currentPage: page,
+    limit,
+    numberOfPages,
+  };
+
+  return pagination;
+};
