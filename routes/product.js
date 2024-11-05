25e466f5b0298862d96fce53de4f972b1b1ac6f2
Soha Ali <sohaali710@gmail.com>
2024-11-05 23:27:00 +0200

    Add auth

--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const productController = require('../controller/product');
+const productController = require("../controller/product");
 const adminMiddleware = require("../middleware/admin");
 // CRUD operations for products
-router.post('/', productController.createProduct);       // Create a new product
-router.get('/', productController.getProducts);          // Get all products
-router.get('/:productId', productController.getProductById); // Get a single product by ID
-router.put('/:productId', productController.updateProduct);  // Update product by ID
-router.delete('/:productId', productController.deleteProduct); // Delete product by ID
-router.get('/get-products/:categoryId', productController.getProductsByCategory);
-router.get('/get-products-search/:query', productController.getProductsByName);
+router.post("/", adminMiddleware.isAuth, productController.createProduct); // Create a new product
+router.get("/", productController.getProducts); // Get all products
+router.get("/:productId", productController.getProductById); // Get a single product by ID
+router.put(
+  "/:productId",
+  adminMiddleware.isAuth,
+  productController.updateProduct
+); // Update product by ID
+router.delete(
+  "/:productId",
+  adminMiddleware.isAuth,
+  productController.deleteProduct
+); // Delete product by ID
+router.get(
+  "/get-products/:categoryId",
+  productController.getProductsByCategory
+);
+router.get("/get-products-search/:query", productController.getProductsByName);
 module.exports = router;
--- routes/storeOrders.js
+++ routes/storeOrders.js
@@ -1,15 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const orderController = require('../controller/storeOrders');
-const userMiddlewares = require('../middleware/user');
+const orderController = require("../controller/storeOrders");
+const userMiddlewares = require("../middleware/user");
 const adminMiddleware = require("../middleware/admin");
 // CRUD operations for orders
-router.post('/', orderController.createOrder);          // Create a new order
-router.get('/', orderController.getOrders);             // Get all orders
-router.get('/:orderId', orderController.getOrderById);  // Get a single order by ID
-router.put('/:orderId', orderController.updateOrderStatus);
-router.get('/user-orders/:userId', orderController.getUserOrders);
+router.post("/", userMiddlewares.isAuth, orderController.createOrder); // Create a new order
+router.get("/", adminMiddleware.isAuth, orderController.getOrders); // Get all orders
+router.get("/:orderId", orderController.getOrderById); // Get a single order by ID
+router.put(
+  "/:orderId",
+  adminMiddleware.isAuth,
+  orderController.updateOrderStatus
+);
+router.get(
+  "/user-orders/:userId",
+  userMiddlewares.isAuth,
+  orderController.getUserOrders
+);
 // Update order status by ID
-router.delete('/:orderId', orderController.deleteOrder); // Delete an order by ID
+router.delete("/:orderId", adminMiddleware.isAuth, orderController.deleteOrder); // Delete an order by ID
 
 module.exports = router;
