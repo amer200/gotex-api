ed743c163912ae6c23a851e958c7d97c071ad63b
Soha Ali <sohaali710@gmail.com>
2024-11-07 16:06:46 +0200

    Add Auth to cart routes

--- routes/cart.js
+++ routes/cart.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const cartController = require('../controller/cart');
+const cartController = require("../controller/cart");
+const userMiddlewares = require("../middleware/user");
 
 // Cart routes
-router.post('/add', cartController.addToCart);        // Add item to cart
-router.get('/:userId', cartController.getCart);       // Get user's cart
-router.put('/update', cartController.updateCartItem); // Update item quantity in cart
-router.delete('/remove', cartController.removeFromCart); // Remove item from cart
-router.delete('/clear', cartController.clearCart);    // Clear the cart
+router.post("/add", userMiddlewares.isAuth, cartController.addToCart); // Add item to cart
+router.get("/", userMiddlewares.isAuth, cartController.getCart); // Get user's cart
+router.put("/update", userMiddlewares.isAuth, cartController.updateCartItem); // Update item quantity in cart
+router.delete("/remove", userMiddlewares.isAuth, cartController.removeFromCart); // Remove item from cart
+router.delete("/clear", userMiddlewares.isAuth, cartController.clearCart); // Clear the cart
 
 module.exports = router;
